5c439aa7828b04b120186e388f43d82ddc166bd6
Zile Zhou <734957793@qq.com>
2023-09-04 16:19:23 +0800

    [chaos]:修改security6的跨域问题

--- chaos-common/chaos-common-security/src/main/java/com/chaos/common/security/config/SecurityConfig.java
+++ chaos-common/chaos-common-security/src/main/java/com/chaos/common/security/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -34,7 +35,8 @@ public class SecurityConfig {
 
   @Bean
   SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.authorizeHttpRequests(
+    http.csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(
             authorize ->
                 authorize
                     .requestMatchers(securityIgnoreUrl.getUrls())
